e3036539eca506d41527c7d3b1c60026d51bb2f0
Vladimir Pogiba <Vovka00821@yandex.ru>
2022-08-10 13:38:48 +0400

    Fourth task reload

---
+++ FourthTaskReload/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FourthTaskReload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, birthday, zodiacSing, country;
+            Console.WriteLine("Как вас зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Какая ваша дата рождения?");
+            birthday = Console.ReadLine();
+            Console.WriteLine("Какой ваш знак зодиака?");
+            zodiacSing = Console.ReadLine();
+            Console.WriteLine("Откуда вы родом?");
+            country = Console.ReadLine();
+            Console.WriteLine("Доброго времени суток {0}!\nВы родились: {1}.\nВаш знак зодиака {2}.\nВаша родина это {3}.", name, birthday, zodiacSing, country);
+        }
+    }
+}
