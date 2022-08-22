0e64b14743210980ef3ae3e71e562c9450dcfbe0
Vamiro <55505126+Vamiro@users.noreply.github.com>
2022-08-22 18:10:58 +0300

    FouthTask classes

---
+++ FourthTask/Birthday.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FourthTask
+{
+    class Birthday
+    {
+        public DateTime DateOfBirthday { get; }
+
+        public Birthday() {
+            DateOfBirthday = ParseInputBirthday();
+        }
+        
+        public DateTime ParseInputBirthday()
+        {
+            DateTime parsedDateOfBirthday;
+            string inputDate;
+
+            do
+            {
+                Console.WriteLine("Укажите Вашу дату рождения в формате дд.мм.гггг:");
+                inputDate = Console.ReadLine();
+            }
+            while (!DateTime.TryParseExact(inputDate, "dd.MM.yyyy", null, DateTimeStyles.None, out parsedDateOfBirthday));
+            return parsedDateOfBirthday;
+        }
+
+        public MonthsNames GetMonthName()
+        {
+            MonthsNames monthName = (MonthsNames)DateOfBirthday.Month;
+            return monthName;
+        }
+
+        public int CalculateAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - DateOfBirthday.Year;
+
+            if (DateOfBirthday.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+
+        public void PrintDate()
+        {
+            Console.WriteLine(DateOfBirthday);
+        }
+
+    }
+}
---
+++ FourthTask/ZodiacSing.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace FourthTask
+{
+    class ZodiacSing
+    {
+        DateTime DateOfBirthday { get; }
+
+        public ZodiacSing(DateTime DateOfBirthday)
+        {
+            this.DateOfBirthday = DateOfBirthday;
+        }
+
+        public ZodiacSings SearchZodiacSingName()
+        {
+            int month = DateOfBirthday.Month;
+            int day = DateOfBirthday.Day;
+
+            switch (month)
+            {
+                case 1:
+                    if (day <= 19)
+                        return (ZodiacSings)12;
+                    else
+                        return (ZodiacSings)1;
+
+                case 2:
+                    if (day <= 18)
+                        return (ZodiacSings)1;
+                    else
+                        return (ZodiacSings)2;
+                case 3:
+                    if (day <= 20)
+                        return (ZodiacSings)2;
+                    else
+                        return (ZodiacSings)3;
+                case 4:
+                    if (day <= 19)
+                        return (ZodiacSings)3;
+                    else
+                        return (ZodiacSings)4;
+                case 5:
+                    if (day <= 20)
+                        return (ZodiacSings)4;
+                    else
+                        return (ZodiacSings)5;
+                case 6:
+                    if (day <= 20)
+                        return (ZodiacSings)5;
+                    else
+                        return (ZodiacSings)6;
+                case 7:
+                    if (day <= 22)
+                        return (ZodiacSings)6;
+                    else
+                        return (ZodiacSings)7;
+                case 8:
+                    if (day <= 22)
+                        return (ZodiacSings)7;
+                    else
+                        return (ZodiacSings)8;
+                case 9:
+                    if (day <= 22)
+                        return (ZodiacSings)8;
+                    else
+                        return (ZodiacSings)9;
+                case 10:
+                    if (day <= 22)
+                        return (ZodiacSings)9;
+                    else
+                        return (ZodiacSings)10;
+                case 11:
+                    if (day <= 21)
+                        return (ZodiacSings)10;
+                    else
+                        return (ZodiacSings)11;
+                case 12:
+                    if (day <= 21)
+                        return (ZodiacSings)11;
+                    else
+                        return (ZodiacSings)12;;
+            }
+            return 0;
+        }
+    }
+}
